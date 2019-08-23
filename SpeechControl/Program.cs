7e2d47f70b8166ad43d2597c9aeeb6928e241d72
Marianne Linardos <mlinardos.edu@gmail.com>
2019-08-23 12:51:35 -0400

    Add project files.

---
+++ SpeechControl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change to different voice
+
+            // Match voice in War Games
+
+            // Change rate of voice
+
+
+        }
+    }
+}
